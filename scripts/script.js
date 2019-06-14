e7e07fc814bd379c5cf68eb1ccfeaa606e1cb39a
cannizarro <asrarul97@gmail.com>
2019-06-15 00:10:26 +0530

    Added loader

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
 (function($) {
   $(document).ready(function() {
     // hide .navbar first
